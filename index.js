8387b4dcfdfe0f2c5d098ffe52efbd99de78bd6a
Jason Viger <jason.viger@gmail.com>
2018-07-09 14:39:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function takeANumber(katzDeliLine,name){
 }
 
 function nowServing(katzDeliLine){
-  
-  return katzDeliLine[0];
+  var next = katzDeliLine[0];
   katzDeliLine.shift();
+  return katzDeliLine[0];
+  
   
 }
